1646da7a2663a628dea223f3b6d5c0869ba97662
Lois Chen <a8283626@gmail.com>
2023-07-15 03:23:04 +0800

    style: modify text style, add image config,

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -37,7 +37,7 @@ export default function Home() {
     PointData
   >(() => (`/api/getPointData`), fetcher)
 
-  if (error) return <div>{error.message}</div>
+  if (error) return <p>維護中...</p>
   if (!data) return null
 
   return (
@@ -53,36 +53,40 @@ export default function Home() {
       </header>
       <main className="flex flex-col items-center ">
         <div className="bg-[#1E2129] rounded-md w-4/5">
-          <div className="m-5 flex flex-col h-20">
-            <h1 className="text-lg">ALPHA Points Leaderboard</h1>
-            <ul>
-              <li>此 Leaderboard 記錄每人每月獲得的 ALPHA Points 數，數據會於每月 1 號更新（歸零重新計算）</li>
-              <li>若想查詢自己的排名，請按「Ctrl/Command + F」，並輸入 Discord 暱稱即可找到對應的欄位</li>
+          <div className="m-5 flex flex-col h-30">
+            <h1 className="mb-2 text-2xl font-semibold">ALPHA Points Leaderboard</h1>
+            <ul className="mb-5">
+              <li className="mb-2">🎃此 Leaderboard 記錄每人每月獲得的 ALPHA Points 數，數據會於每月 1 號更新（歸零重新計算）</li>
+              <li>🎃若想查詢自己的排名，請按「Ctrl/Command + F」，並輸入 Discord 暱稱即可找到對應的欄位</li>
             </ul>
-          </div>
-          <div className="mx-5">
-            <hr className="mb-5" />
-            <div className="flex justify-between mb-5">
+            <hr />
+            <div className="flex justify-between mt-5">
+              <p>Rank</p>
               <p>User</p>
-              <p>Point</p>
+              <p>Points</p>
             </div>
+          </div>
+          <div className="mx-10">
             {isLoading && <p className="mb-5">Loading...</p>}
             {data.data.length !== 0
-              ? data.data.map((u, index) => (
-                <div key={index} className="flex justify-between items-center mb-4">
-                  <div className="flex gap-4 mt-5 items-center">
-                    <Image
-                      className="rounded-full bg-white"
-                      src={"/AC_Logo.png"}
-                      alt="logo"
-                      width={50}
-                      height={50}
-                    />
-                    <p>{u.name}</p>
+              ? data.data
+                .sort((a, b) => { return b.point - a.point })
+                .map((u, index) => (
+                  <div key={index} className="flex justify-between items-center mb-4">
+                    <p>{index + 1}</p>
+                    <div className="flex gap-4 mt-5 items-center">
+                      <Image
+                        className="rounded-full bg-white"
+                        src={u.avatarURL}
+                        alt="logo"
+                        width={50}
+                        height={50}
+                      />
+                      <p>{u.name}</p>
+                    </div>
+                    <p>{u.point}</p>
                   </div>
-                  <p>{u.point}</p>
-                </div>
-              ))
+                ))
               : <div className="flex justify-between items-center mb-4">
                 <p>這個月還沒有人得到 Point</p>
               </div>
