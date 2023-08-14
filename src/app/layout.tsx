de51df9f5c2075e166758ef3cec478b0487cf7f2
loischen68 <a8283626@gmail.com>
2023-08-14 16:12:31 +0800

    style: modify table

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,22 +1,24 @@
-import './globals.css'
-import { Inter } from 'next/font/google'
+import "./globals.css";
+import { Inter } from "next/font/google";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: 'Alpha Point LeaderBoard',
-  description: 'Alpha Point LeaderBoard',
-}
+  title: "Alpha Point LeaderBoard",
+  description: "Alpha Point LeaderBoard",
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="zh-tw">
       <link rel="icon" href="/AC_Logo.png" />
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className} suppressHydrationWarning={true}>
+        {children}
+      </body>
     </html>
-  )
+  );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,44 +1,54 @@
-'use client'
+"use client";
 
-import Image from 'next/image'
-import useSWR from 'swr'
-import { useState } from 'react'
+import Image from "next/image";
+import useSWR from "swr";
+import {
+  Box,
+  Table,
+  TableContainer,
+  Tbody,
+  Td,
+  Th,
+  Thead,
+  Tr,
+} from "@chakra-ui/react";
 
 type Data = {
-  id: string
-  name: string
-  avatarURL: string
-  period: string
-  discordId: string
-  point: number
-}
+  id: string;
+  name: string;
+  avatarURL: string;
+  period: string;
+  discordId: string;
+  point: number;
+};
 
 type PointData = {
-  data: Data[]
-  offset: number
-  pageSize: number
-  totalPages: number
-  currentPage: number
-  totalDataCount: number
-}
+  data: Data[];
+  offset: number;
+  pageSize: number;
+  totalPages: number;
+  currentPage: number;
+  totalDataCount: number;
+};
 
 const fetcher = async (url: string) => {
-  const res = await fetch(url)
-  const data = await res.json()
+  const res = await fetch(url);
+  const data = await res.json();
 
   if (res.status !== 200) {
-    throw new Error(data.message)
+    throw new Error(data.message);
   }
-  return data
-}
+  return data;
+};
 
 export default function Home() {
-  const { data, error, isLoading } = useSWR<
-    PointData
-  >(() => (`/api/getPointData`), fetcher)
+  const { data, error, isLoading } = useSWR<PointData>(
+    () => `/api/getPointData`,
+    fetcher
+  );
 
-  if (error) return <p>維護中...</p>
-  if (!data) return null
+  if (error) return <p>維護中...</p>;
+  if (!data) return null;
 
   return (
     <>
@@ -51,49 +61,75 @@ export default function Home() {
           height={50}
         />
       </header>
-      <main className="flex flex-col items-center ">
-        <div className="bg-[#1E2129] rounded-md w-4/5">
-          <div className="m-5 flex flex-col h-30">
-            <h1 className="mb-2 text-2xl font-semibold">ALPHA Points Leaderboard</h1>
+      <Box display={"flex"} justifyContent={"center"} alignItems={"center"}>
+        <Box className="bg-[#1E2129] rounded-md w-4/5">
+          <Box
+            display={"flex"}
+            flexDirection={"column"}
+            alignItems={"center"}
+            margin={"10px"}
+          >
+            <h1 className="mb-2 text-2xl font-semibold">
+              ALPHA Points Leaderboard
+            </h1>
             <ul className="mb-5">
-              <li className="mb-2">🎃此 Leaderboard 記錄每人每月獲得的 ALPHA Points 數，數據會於每月 1 號更新（歸零重新計算）</li>
-              <li>🎃若想查詢自己的排名，請按「Ctrl/Command + F」，並輸入 Discord 暱稱即可找到對應的欄位</li>
+              <li className="mb-2">
+                🎃此 Leaderboard 記錄每人每月獲得的 ALPHA Points
+                數，數據會於每月 1 號更新（歸零重新計算）
+              </li>
+              <li>
+                🎃若想查詢自己的排名，請按「Ctrl/Command + F」，並輸入 Discord
+                暱稱即可找到對應的欄位
+              </li>
             </ul>
             <hr />
-            <div className="flex justify-between mt-5">
-              <p>Rank</p>
-              <p>User</p>
-              <p>Points</p>
-            </div>
-          </div>
-          <div className="mx-10">
-            {isLoading && <p className="mb-5">Loading...</p>}
-            {data.data.length !== 0
-              ? data.data
-                .sort((a, b) => { return b.point - a.point })
-                .map((u, index) => (
-                  <div key={index} className="flex justify-between items-center mb-4">
-                    <p>{index + 1}</p>
-                    <div className="flex gap-4 mt-5 items-center">
-                      <Image
-                        className="rounded-full bg-white"
-                        src={u.avatarURL}
-                        alt="logo"
-                        width={50}
-                        height={50}
-                      />
-                      <p>{u.name}</p>
-                    </div>
-                    <p>{u.point}</p>
-                  </div>
-                ))
-              : <div className="flex justify-between items-center mb-4">
-                <p>這個月還沒有人得到 Point</p>
-              </div>
-            }
-          </div>
-        </div>
-      </main>
+          </Box>
+          <TableContainer
+            display={"flex"}
+            justifyContent={"center"}
+            marginTop={"10px"}
+          >
+            <Table className="rounded-md w-4/5">
+              <Thead className="text-left">
+                <Tr>
+                  <Th className="text-center">Rank</Th>
+                  <Th>User</Th>
+                  <Th className="text-center">🧡 Points</Th>
+                </Tr>
+              </Thead>
+              <Tbody className="text-left">
+                {isLoading && <p className="mb-5">Loading...</p>}
+                {data.data.length !== 0 ? (
+                  data.data
+                    .sort((a, b) => {
+                      return b.point - a.point;
+                    })
+                    .map((u, index) => (
+                      <Tr key={index}>
+                        <Td className="text-center">{index + 1}</Td>
+                        <Td className="flex gap-4 mt-5 items-center">
+                          <Image
+                            className="rounded-full bg-white"
+                            src={u.avatarURL}
+                            alt="logo"
+                            width={50}
+                            height={50}
+                          />
+                          {u.name}
+                        </Td>
+                        <Td className="text-center">{u.point}</Td>
+                      </Tr>
+                    ))
+                ) : (
+                  <Box className="flex justify-between items-center mb-4">
+                    <p>這個月還沒有人得到 Point</p>
+                  </Box>
+                )}
+              </Tbody>
+            </Table>
+          </TableContainer>
+        </Box>
+      </Box>
     </>
-  )
+  );
 }
